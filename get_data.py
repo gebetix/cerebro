845e23966528e82eca9c23a02722b92da2e2d19c
Aleksandr Bukin <gebetix@gmail.com>
2016-03-21 13:13:52 +0500

    Return of Embedding images

--- get_data.py
+++ get_data.py
@@ -5,11 +5,12 @@ import json
 def get_images(lat, lon):
     distance = 500
 
-    url = "https://api.instagram.com/v1/media/search?lat=%s&lng=%s&distance=%s&client_id=3d56e75ea0ae4b96aa459aa22530a519"
+    url_search = "https://api.instagram.com/v1/media/search?lat=%s&lng=%s&distance=%s&client_id=3d56e75ea0ae4b96aa459aa22530a519"
+    url_oembed = "https://api.instagram.com/oembed?url=%s"
 
-    data = json.loads(urlopen(url % (lat, lon, distance)).read().decode('utf8'))
-    data = data["data"]
+    data_search = json.loads(urlopen(url_search % (lat, lon, distance)).read().decode('utf8'))
+    data_search = data_search["data"]
 
-    for image in data:
-        yield image['images']['standard_resolution']['url']
-
+    for image in data_search:
+        data_oembed = json.loads(urlopen(url_oembed % (image['link'])).read().decode('utf8'))
+        yield data_oembed["html"]
